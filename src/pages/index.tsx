e7a13d4baeee6343d3b7210e40b9513dd9b65f50
andiartori <rizkyandiarto@gmail.com>
2024-11-19 14:13:12 +0700

    Finalization

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -326,7 +326,7 @@ function Home() {
 								<div
 									key={index}
 									data-aos="fade-up"
-									className="bg-gray-100 p-4 shadow-md flex flex-col items-center hover:scale-125 hover:shadow-xl hover:bg-gray-200 transition-transform transform"
+									className="bg-gray-100 p-4 shadow-md flex flex-col items-center transform hover:scale-125 hover:shadow-xl hover:bg-gray-200 transition-transform duration-300"
 								>
 									{/* Bagian gambar */}
 									<div className="w-full h-56 mb-4">
